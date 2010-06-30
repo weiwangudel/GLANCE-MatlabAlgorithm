7eb9714159827c8b9d514686fb6ceea7b6320327
Wei Wang <weiwang.udel@gmail.com>
2010-06-30 01:27:09 +0000

    added more variances
    git-svn-id: file:///home/weiwang/gwu_svn_repo_08_24_2010/matlab@3 e8ef62b9-6a3f-410e-b737-878857538f96

--- super_variance_est.m
+++ super_variance_est.m
@@ -7,8 +7,8 @@ fs_filepart_32 = file_data;
 nefolders = unique(fs_dirpart_32(:,2));
 efolders = setdiff(1:size(fs_dirpart_32,1), nefolders);
 fnum = (histc(fs_filepart_32(:,2),[-1:size(fs_dirpart_32,1)-1]+0.5));
-
-pilottrials = 300;
+real_count = size(fs_filepart_32, 1);
+pilottrials = 3000;
 
 'ok'
 
@@ -40,18 +40,33 @@ for drilldown = 1:pilottrials
     curfnum = fnum(curd+1); % file number under current dir	
     level_est_times(level) = level_est_times(level) + 1;
 	square_of_sum(level) = (square_of_sum(level) + curfnum / curp);
-	sum_of_square(level) = sum_of_square(level) + (curfnum/curp).^2;
+	sum_of_square(level) = 0 + (curfnum/curp).^2;
     sum1 = sum1 + curfnum/curp;
-    
+	    
 	% compute variance --sum up all the level variances
 %	sum_of_square
 %	square_of_sum
-    variance=sum_of_square/pilottrials - (square_of_sum/pilottrials).^2;
-	variance=sum(variance)
+    third_variance=sum_of_square - (square_of_sum/drilldown).^2;
+	third_std_variance(drilldown)=sqrt(sum(third_variance))
+	
+	% compute real variance
+	individual_count_res(drilldown) = sum1;
+	sum1 = 0;
+	first_variance = (individual_count_res - real_count).^2;
+	first_std_variance(drilldown) = sqrt(sum(first_variance))
+	
+	%compute accumulating variance
+	count_res(drilldown) = sum(individual_count_res)/drilldown;
+	accu_first_variance = (count_res - real_count).^2;
+	accu_first_std_variance(drilldown) = sqrt(sum(accu_first_variance))
+
+	%compute second variance
+	our_count = count_res(drilldown);
+	second_variance = (individual_count_res - our_count).^2;
+	second_std_variance(drilldown) = sqrt(sum(second_variance))	
 	
 end;
-%square_of_sum / pilottrials
-%sum1/pilottrials
 ' drill down ok'
-
+count_res(pilottrials)
+mean(individual_count_res)
 
